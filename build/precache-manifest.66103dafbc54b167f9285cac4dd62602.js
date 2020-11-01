03cf042b375eedca352fbf3a0681cc43e70e0659
rupambumba <bybumba@gmail.com>
2020-11-01 16:42:17 +0530

    deploymant 0.0.1

--- build/precache-manifest.66103dafbc54b167f9285cac4dd62602.js
+++ build/precache-manifest.66103dafbc54b167f9285cac4dd62602.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3fb8fd91aea54228a96743b757317aba",
+    "revision": "a5026b2de0c80ac1817036fb13b03b60",
     "url": "/index.html"
   },
   {
-    "revision": "2b6d8c9aafc263cb28eb",
+    "revision": "5e643eb0d832518c884b",
     "url": "/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "454005d5353caf7ae630",
-    "url": "/static/js/2.e3bf9a43.chunk.js"
+    "revision": "48517ace10b2888cde0f",
+    "url": "/static/js/2.edce8114.chunk.js"
   },
   {
     "revision": "8a9a2ae4a25f202f52818f8f8074424e",
-    "url": "/static/js/2.e3bf9a43.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.edce8114.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2b6d8c9aafc263cb28eb",
-    "url": "/static/js/main.bf459905.chunk.js"
+    "revision": "5e643eb0d832518c884b",
+    "url": "/static/js/main.bc3171d1.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b3b65abdf61e711216c2011dce8875b2.js"
+  "/precache-manifest.66103dafbc54b167f9285cac4dd62602.js"
 );
 
 self.addEventListener('message', (event) => {
