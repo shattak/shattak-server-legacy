caa78a455c5755631e0c804e81997ed9115628da
rupambumba <bybumba@gmail.com>
2021-02-14 22:21:43 +0530

    added Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -35,6 +35,25 @@ app.use(express.json());
  var cors = require("cors");
  app.use(cors());
 
+ // Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 //  const MongoClient = require('mongodb').MongoClient;
 //  const uri = "mongodb+srv://shattak-api:TG8MVUUSGcgMNn8H@shattak.gwo47.mongodb.net/<dbname>?retryWrites=true&w=majority";
