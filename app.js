a3faeb03e9479509f8922c05f12f51718750e986
rupambumba <bybumba@gmail.com>
2021-02-14 22:42:57 +0530

    added https://www.shattak.com  in Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(function (req, res, next) {
 
   console.log("x-x-x-x-x-x-x-x-x-x-x-cors x-x-x-x-x-x-x-x-x-x-x-x-x-x ");
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.shattak.com');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
