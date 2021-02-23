d236a89466abb0970c02b835afe11449ae2a7abb
rupambumba <bybumba@gmail.com>
2021-02-23 14:23:28 +0530

    added  created at on auth model

--- api/models/authentications.js
+++ api/models/authentications.js
@@ -3,26 +3,29 @@ const Schema = mongoose.Schema;
 
 const authentications = new Schema({
   _id: mongoose.Schema.Types.ObjectId,
-    _users_id: {
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  _users_id: {
     type: Schema.Types.ObjectId,
     required: true,
     ref: "users",
-    unique: true
+    unique: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   email_verified: {
     type: Boolean,
-    default: false
+    default: false,
   },
   password: {
     type: String,
     required: true,
   },
-  
 });
- 
+
 module.exports = mongoose.model("authentications", authentications);
