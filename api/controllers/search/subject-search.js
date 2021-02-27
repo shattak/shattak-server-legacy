25a057c1757eefc38f985cd8186deaa7f464f2e8
rupambumba <bybumba@gmail.com>
2021-02-27 23:53:58 +0530

    addded notes search

--- api/controllers/search/subject-search.js
+++ api/controllers/search/subject-search.js
@@ -3,11 +3,32 @@ const subjectsDB = require("../../models/education-structure/subjects");
 exports.get_subject_search = (req, res, next) => {
   console.log("[DEBUG 10]\t" + "get_subject_search");
 
-  const SearchQuery = req.query.q;
+  var SearchQuery = req.query.q;
+  var PageLimit = 10;
+  var pgnum = req.query.pg;
+  var skiping = (pgnum - 1) * PageLimit;
 
-  subjectsDB()
+  var regexQuery = {
+    name: new RegExp(SearchQuery, "i"),
+  };
 
-  res.status(200).json({
-    mag: "get_subject_search",
-  });
+  if (SearchQuery.length === 0) {
+    const err = new Error("Search Query length is zero");
+    return next(err);
+  } else {
+    subjectsDB
+      .find(regexQuery)
+      .skip(skiping)
+      .limit(PageLimit)
+      .exec()
+      .then((result) => {
+        res.status(200).json({
+          result,
+        });
+      })
+      .catch((error) => {
+        console.log("error");
+        return next(error);
+      });
+  }
 };
