d41f1ddc333b92e992cd73d01341785d2a679202
rupambumba <bybumba@gmail.com>
2021-02-28 00:43:09 +0530

    added useer scaech

--- api/controllers/search/users-search.js
+++ api/controllers/search/users-search.js
@@ -3,11 +3,36 @@ const usersDB = require("../../models/users");
 exports.get_users_search = (req, res, next) => {
   console.log("[DEBUG 10]\t" + "get_users_search");
 
-  const SearchQuery = req.query.q;
+  var SearchQuery = req.query.q;
+  var PageLimit = 10;
+  var pgnum = req.query.pg;
+  var skiping = (pgnum - 1) * PageLimit;
 
-  usersDB()
+  var regexQuery = {
+    $or: [
+      { first_name: new RegExp(SearchQuery, "i") },
+      { last_name: new RegExp(SearchQuery, "i") },
+      { users_name: new RegExp(SearchQuery, "i") },
+    ],
+  };
 
-  res.status(200).json({
-    mag: "get_users_search",
-  });
+  if (SearchQuery.length === 0) {
+    const err = new Error("Search Query length is zero");
+    return next(err);
+  } else {
+    usersDB
+      .find(regexQuery)
+      .skip(skiping)
+      .limit(PageLimit)
+      .exec()
+      .then((result) => {
+        res.status(200).json({
+          result,
+        });
+      })
+      .catch((error) => {
+        console.log("error");
+        return next(error);
+      });
+  }
 };
