af4b8410a79ee5143aaf69fde129590081e18668
rupambumba <bybumba@gmail.com>
2021-02-27 23:42:22 +0530

    added institute scarch funttion

--- api/controllers/search/institute-search.js
+++ api/controllers/search/institute-search.js
@@ -1,14 +1,38 @@
-const institutesDB = require('../../models/education-structure/institutes');
+const institutesDB = require("../../models/education-structure/institutes");
 
+exports.get_institute_search = (req, res, next) => {
+  console.log("[DEBUG 10]\t" + "get_institute_search");
 
-exports.get_institute_search = (req,res,next)=>{
-    console.log("[DEBUG 10]\t"+"get_institute_search");
+  var SearchQuery = req.query.q;
+  var PageLimit = 10;
+  var pgnum = req.query.pg;
+  var skiping = (pgnum - 1) * PageLimit;
 
-    const SearchQuery = req.query.q;
+  var regexQuery = {
+    name: new RegExp(SearchQuery, "i"),
+  };
 
+  console.log(typeof SearchQuery + "====" + SearchQuery);
+  console.log(regexQuery);
+
+  if (SearchQuery.length === 0) {
+    const err = new Error("Search Query length is zero");
+    return next(err);
+  } else {
     institutesDB
-    
-    res.status(200).json({
-        mag :"get_institute_search"
-    })
-}
+      .find(regexQuery)
+      .skip(skiping)
+      .limit(PageLimit)
+      .exec()
+      .then((result) => {
+        res.status(200).json({
+          result,
+        });
+      })
+      .catch((error) => {
+        console.log("error");
+        return next(error);
+      });
+  }
+
+};
