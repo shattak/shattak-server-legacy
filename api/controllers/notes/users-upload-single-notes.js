dc47d3bafd5b6ce5b6b928eb7a889dafee1d9bab
rupambumba <bybumba@gmail.com>
2021-04-02 11:21:15 +0530

    contentType in multers3 file.minetype

--- api/controllers/notes/users-upload-single-notes.js
+++ api/controllers/notes/users-upload-single-notes.js
@@ -33,7 +33,10 @@ var s3 = new aws.S3({
 var bucketStorage = multerS3({
   s3: s3,
   bucket: process.env.S3Bucket,
-  contentType: multerS3.AUTO_CONTENT_TYPE,
+  // contentType: multerS3.AUTO_CONTENT_TYPE,
+  contentType: function (req, file, cb) {
+    cb(null, file.mimetype)
+  },
   metadata: function (req, file, cb) {
     cb(null, { fieldName: file.fieldname });
   },
